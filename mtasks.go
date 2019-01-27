6ed9885e0c6b5dd4e5d962cfb5400fba0bc50189
Sushant Srivastava <sushant.srivastav@gmail.com>
2019-01-27 13:25:15 +0530

    Added TaskManager interface
    Signed-off-by: Sushant Srivastava <sushant.srivastav@gmail.com>

--- mtasks.go
+++ mtasks.go
@@ -1,6 +1,14 @@
 package mtasks
 
+// Tasks represent a common task that needs to be done
 type Task struct {
 	Title       string
 	Description string
+}
+
+// TaskManager represents an interface which manages task
+type TaskManager interface {
+	Create() (Task, error)
+	Edit(t Task) (bool, error)
+	MarkDone(t Task)
 }
